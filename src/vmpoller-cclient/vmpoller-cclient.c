043685f45c0009c94a0041942641f79805be93ab
Marin Atanasov Nikolov <dnaeon@gmail.com>
2013-09-05 14:47:14 +0300

    Add BSD license to the C client

--- src/vmpoller-cclient/vmpoller-cclient.c
+++ src/vmpoller-cclient/vmpoller-cclient.c
@@ -1,3 +1,30 @@
+/*-
+ * Copyright (c) 2013 Marin Atanasov Nikolov <dnaeon@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer
+ *    in this position and unchanged.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 /* 
  * vmpoller-cclient is a C client intended to be used for polling/discovering
  * information from a vSphere host (ESX or vCenter server).
@@ -8,6 +35,7 @@
  * The received reply is in turn printed to stdout, which allows Zabbix to catch it,
  * therefore we don't use printf(3) here to output anything except for the reply.
  */
+
 
 #include <stdio.h>
 #include <stdbool.h>
