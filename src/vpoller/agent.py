8edbe2a6c326c2734dd7491a387835abf933f28f
Marin Atanasov Nikolov <dnaeon@gmail.com>
2014-01-08 14:07:58 +0200

    The vSphereAgents always return result in JSON
    * Also remove for now the Zabbix parts until we have a better way
       to add support for Zabbix, perhaps as a separate module

--- src/vpoller/agent.py
+++ src/vpoller/agent.py
@@ -56,28 +56,26 @@ class VSphereAgent(VConnector):
 
         Example client message to get a host property could be:
 
-        msg = { "type"     : "hosts",
-                "hostname" : "sof-vc0-mnik",
-                "name"     : "sof-dev-d7-mnik",
+            {
+                "method":    "host.poll",
+                "hostname":  "vc01-test.example.org",
+                "name"     : "esxi01-test.example.org",
                 "property" : "hardware.memorySize"
-              }
+            }
         
         Args:
-            msg (dict): The client message
-
-        Returns:
-            The requested property value
+            msg (dict): The client message to process
 
         """
         # Sanity check for required attributes in the message
-        if not all(k in msg for k in ("type", "hostname", "name", "property")):
-            return "{ \"success\": -1, \"msg\": \"Missing message properties (e.g. type/hostname/name/property)\" }"
+        if not all(k in msg for k in ("name", "property")):
+            return "{ \"success\": -1, \"msg\": \"Missing message properties (e.g. name/property)\" }"
 
         # Check if we are connected first
         if not self.viserver.is_connected():
             self.reconnect()
         
-        # Search is done by using the 'name' property of the ESX Host as
+        # Search is done by using the 'name' property of the ESXi Host as
         # this uniquely identifies the host
         #
         # Check the vSphere Web Services SDK API for more information on the properties
@@ -86,18 +84,6 @@ class VSphereAgent(VConnector):
         #
         property_names = ['name', msg['property']]
 
-        # Some of the properties we process need to be converted to a Zabbix-friendly value
-        # before passing the value back to the Zabbix Agents
-        zbx_helpers = {
-            'summary.quickStats.overallCpuUsage':            lambda p: p * 1048576, # The value we return is in Hertz
-            'summary.quickStats.overallMemoryUsage':         lambda p: p * 1048576, # The value we return is in Bytes
-            'summary.quickStats.distributedMemoryFairness':  lambda p: p * 1048576, # The value we return is in Bytes
-            'runtime.inMaintenanceMode':                     lambda p: int(p),      # The value we return is integer
-            'summary.config.vmotionEnabled':                 lambda p: int(p),      # The value we return is integer
-            'summary.rebootRequired':                        lambda p: int(p),      # The value we return is integer
-            'runtime.bootTime':                              lambda p: strftime('%Y-%m-%d %H:%M:%S', p),
-        }
-            
         logging.info('[%s] Retrieving %s for host %s', self.hostname, msg['property'], msg['name'])
 
         # Get the properties for all registered hosts
@@ -122,15 +108,10 @@ class VSphereAgent(VConnector):
                 break
         else:
             return "{ \"success\": -1, \"msg\": \"Unable to find ESXi host %s\" }" % msg["name"]
-        
-        # Get the property value
-        val = d[msg["property"]] if d.get(msg["property"]) else 0
 
-        # Do we need to convert this value to a Zabbix-friendly one?
-        if msg["property"] in zbx_helpers:
-            val = zbx_helpers[msg["property"]](val)
+        result = "{ \"status\": 0, \"msg\": \"Successfully retrieved property\", \"result\": \"%s\" }" % json.dumps(d)
 
-        return val
+        return result
             
     def get_datastore_property(self, msg):
         """
@@ -138,22 +119,20 @@ class VSphereAgent(VConnector):
 
         Example client message to get a host property could be:
 
-        msg = { "type"    : "datastores",
-                "hostname": "sof-vc0-mnik",
+            {
+                "method":   "datastore.poll",
+                "hostname": "vc01-test.example.org",
                 "info.url": "ds:///vmfs/volumes/5190e2a7-d2b7c58e-b1e2-90b11c29079d/",
                 "property": "summary.capacity"
-              }
+            }
         
         Args:
-            msg (dict): The client message
+            msg (dict): The client message to process
         
-        Returns:
-            The requested property value
-
         """
         # Sanity check for required attributes in the message
-        if not all(k in msg for k in ("type", "hostname", "info.url", "property")):
-            return "{ \"success\": -1, \"msg\": \"Missing message properties (e.g. type/hostname/info.url/property)\" }"
+        if not all(k in msg for k in ("info.url", "property")):
+            return "{ \"success\": -1, \"msg\": \"Missing message properties (e.g. info.url/property)\" }"
 
         # Check if we are connected first
         if not self.viserver.is_connected():
@@ -167,26 +146,6 @@ class VSphereAgent(VConnector):
         #     https://www.vmware.com/support/developer/vc-sdk/
         # 
         property_names = ['info.url', msg['property']]
-
-        # Custom properties, which are not available in the vSphere Web SDK
-        # Keys are the property names and values are a list/tuple of the properties required to
-        # calculate the custom properties
-        custom_zbx_properties = {
-            'ds_used_space_percentage': ['summary.freeSpace', 'summary.capacity']
-        }
-                    
-        # Some of the properties we process need to be converted to a Zabbix-friendly value
-        # before passing the value back to the Zabbix Agents
-        zbx_helpers = {
-            'ds_used_space_percentage': lambda d: round(100 - (float(d['summary.freeSpace']) / float(d['summary.capacity']) * 100), 2),
-        }
-
-        # Check if we have a custom property requested
-        # If we have a custom property we need to append the required properties
-        # and also remove the custom one, otherwise we will get an exception
-        if msg["property"] in custom_zbx_properties:
-            property_names.extend(custom_zbx_properties[msg["property"]])
-            property_names.remove(msg["property"])
 
         logging.info('[%s] Retrieving %s for datastore %s', self.hostname, msg['property'], msg['info.url'])
 
@@ -211,37 +170,31 @@ class VSphereAgent(VConnector):
         else:
             return "{ \"success\": -1, \"msg\": \"Unable to find datastore %s\" }" % msg["info.url"]
 
-        # Do we need to convert this value to a Zabbix-friendly one?
-        if msg["property"] in zbx_helpers:
-            val = zbx_helpers[msg["property"]](d)
-        else:
-            # No need to convert anything
-            return json.dumps({ 'result': d }, indent=4)
-            val = d[msg["property"]] if d.get(msg["property"]) else 0
+        result = "{ \"status\": 0, \"msg\": \"Successfully retrieved property\", \"result\": \"%s\" }" % json.dumps(d)
 
-        return val
-
+        return result
+        
     def discover_hosts(self, msg):
         """
-        Discovers all ESX hosts registered in the VMware vSphere server.
+        Discovers all ESXi hosts registered in the VMware vSphere server.
 
         Example client message to discover all ESXi hosts could be:
 
-        msg = { "type"    : "hosts",
-        	"hostname": "sof-vc0-mnik",
-                "cmd"     : "discover",
+            {
+                "method":   "host.discover",
+                "hostname": "vc01-test.example.org",
                 "zabbix"  : False,
-              }
+            }
 
-        If "zabbix" is set to True then the result will be formatted in a way that the
-        Zabbix Low-Level Discovery protocol can understand and use.
+        If "zabbix" attribute is set to True then the result will be formatted
+        in a way that the Zabbix Low-Level Discovery protocol can understand and use.
 
         For more information about Zabbix Low-Level Discovery, please check the link below:
 
           - https://www.zabbix.com/documentation/2.2/manual/discovery/low_level_discovery
               
         Returns:
-            The returned data is a JSON object, containing the discovered ESX hosts.
+            The returned data is a JSON object, containing the discovered ESXi hosts.
 
         """
         # Check if we are connected first
@@ -254,7 +207,7 @@ class VSphereAgent(VConnector):
         #
         #     https://www.vmware.com/support/developer/vc-sdk/
         #
-        property_names = ['name', 'runtime.powerState', 'summary.managementServerIp']
+        property_names = ['name', 'runtime.powerState']
 
         logging.info('[%s] Discovering ESXi hosts', self.hostname)
 
@@ -267,7 +220,7 @@ class VSphereAgent(VConnector):
             return "{ \"success\": -1, \"msg\": \"Cannot discover ESXi hosts: %s\" }" % e
 
         # Iterate over the results and prepare the JSON object
-        json_data = []
+        data = []
         for item in results:
             # Do we want to return the data in Zabbix LLD format?
             # The names of the properties we return is in Zabbix Macro-like format, e.g.
@@ -282,13 +235,13 @@ class VSphereAgent(VConnector):
                 props = [(p.Name, p.Val) for p in item.PropSet]
 
             d = dict(props)
-            json_data.append(d)
+            data.append(d)
 
         # Return result in Zabbix LLD format
         if msg.get('zabbix'):
-            return json.dumps({ 'data': json_data}, indent=4)
+            return "{ \"data\": %s }" % json.dumps(data)
 
-        return json.dumps({ 'result': json_data}, indent=4)
+        return "{ \"success\": 0, \"msg\": \"Successfully discovered ESXi hosts\", \"result\": %s }" % json.dumps(data)
 
     def discover_datastores(self, msg):
         """
@@ -296,14 +249,14 @@ class VSphereAgent(VConnector):
 
         Example client message to discover all datastores could be:
         
-        msg = { "type"    : "datastores",
-        	"hostname": "sof-vc0-mnik",
-                "cmd"     : "discover",
+            {
+                "method":   "datastore.discover",
+        	"hostname": "vc01-test.example.org",
                 "zabbix"  : False,
-              }
+            }
 
-        If "zabbix" is set to True then the result will be formatted in a way that the
-        Zabbix Low-Level Discovery protocol can understand and use.
+        If "zabbix" attribute is set to True then the result will be formatted
+        in a way that the Zabbix Low-Level Discovery protocol can understand and use.
 
         For more information about Zabbix Low-Level Discovery, please check the link below:
 
@@ -336,7 +289,7 @@ class VSphereAgent(VConnector):
             return "{ \"success\": -1, \"msg\": \"Cannot discover datastores: %s\" }" % e
 
         # Iterate over the results and prepare the JSON object
-        json_data = []
+        data = []
         for item in results:
             # Do we want to return the data in Zabbix LLD format?
             # The names of the properties we return is in Zabbix Macro-like format, e.g.
@@ -351,10 +304,10 @@ class VSphereAgent(VConnector):
                 props = [(p.Name, p.Val) for p in item.PropSet]
 
             d = dict(props)
-            json_data.append(d)
-            
+            data.append(d)
+
         # Return result in Zabbix LLD format
         if msg.get('zabbix'):
-            return "{ \"data\": %s }" % json.dumps(json_data)
+            return "{ \"data\": %s }" % json.dumps(data)
 
-        return "{ \"success\": 0, \"msg\": \"Successfully discovered datastores\", \"result\": %s }" % json.dumps(json_data)
+        return "{ \"success\": 0, \"msg\": \"Successfully discovered datastores\", \"result\": %s }" % json.dumps(data)
