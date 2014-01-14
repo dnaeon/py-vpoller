4a6399cdd809fa8c686422c6939f7d25de178e1f
Marin Atanasov Nikolov <dnaeon@gmail.com>
2014-01-14 12:54:10 +0200

    Add a few more example requests

--- src/misc-tools/request-simulator.sh
+++ src/misc-tools/request-simulator.sh
@@ -13,7 +13,9 @@ while [[ ${i} -lt ${TOTAL_REQUESTS} ]]; do
     # Fire up another request if needed
     if [[ ${running} -lt ${MAX_REQUESTS} ]]; then
 	echo "I: Firing up request ${i} ..."
-	vpoller-client -D -n esx1-evn1_local0 -p summary.capacity -u ds:///vmfs/volumes/4c68dc48-0db9ca38-f0e0-78e7d1e5782e/ -c poll -V vc1-sof2 > /dev/null 2>&1 &
+	vpoller-client -m host.discover -V vc01.example.org -e tcp://localhost:10123 > /dev/null 2>&1 &
+	vpoller-client -m datastore.discover -V vc01.example.org -e tcp://localhost:10123 > /dev/null 2>&1 &
+	vpoller-client -m datastore.poll -p summary.capacity -u ds:///vmfs/volumes/4c68dc48-0db9ca38-f0e0-78e7d1e5782e/ -V vc01.example.org > /dev/null 2>&1 &
 	let i++
     fi
 done
