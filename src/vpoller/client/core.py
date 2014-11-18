83cec43d2296be5b894e82d25aeb4724714dc283
Marin Atanasov Nikolov <dnaeon@gmail.com>
2014-11-18 14:59:58 +0200

    VPollerClient class is now at vpoller.client.core module

---
+++ src/vpoller/client/core.py
@@ -0,0 +1,189 @@
+# Copyright (c) 2013-2014 Marin Atanasov Nikolov <dnaeon@gmail.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer
+#    in this position and unchanged.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+vPoller Client module for the VMware vSphere Poller
+
+"""
+
+import types
+import logging
+
+import zmq
+
+
+class VPollerClient(object):
+    """
+    VPollerClient class
+
+    Defines methods used by clients for sending out message requests
+
+    Sends out messages to a vPoller Proxy or vPoller Worker requesting
+    properties of different vSphere objects, e.g. datastores, hosts, etc.
+
+    Returns:
+        The result message back to the client
+
+    """
+    def __init__(self, endpoint, timeout=3000, retries=3):
+        """
+        Initializes a VPollerClient object
+
+        Args:
+            timeout  (int): Timeout after that number of milliseconds
+            retries  (int): Number of retries
+            endpoint (str): Endpoint we connect the client to
+
+        """
+        self.timeout = timeout
+        self.retries = retries
+        self.endpoint = endpoint
+
+    def run(self, msg):
+        """
+        Main vPoller Client method
+
+        Partially based on the Lazy Pirate Pattern:
+
+        http://zguide.zeromq.org/py:all#Client-Side-Reliability-Lazy-Pirate-Pattern
+
+        Args:
+            msg (dict): The client message to send
+
+        """
+        logging.debug('Endpoint to connect to: %s', self.endpoint)
+        logging.debug('Timeout of request: %s ms', self.timeout)
+        logging.debug('Number of retries: %d', self.retries)
+        logging.debug('Message to be sent: %s', msg)
+
+        self.zcontext = zmq.Context()
+        self.zclient = self.zcontext.socket(zmq.REQ)
+        self.zclient.connect(self.endpoint)
+        self.zclient.setsockopt(zmq.LINGER, 0)
+        self.zpoller = zmq.Poller()
+        self.zpoller.register(self.zclient, zmq.POLLIN)
+        result = None
+
+        while self.retries > 0:
+            logging.debug('Sending client message...')
+
+            # Send our message out
+            self.zclient.send_json(msg)
+            socks = dict(self.zpoller.poll(self.timeout))
+
+            # Do we have a reply?
+            if socks.get(self.zclient) == zmq.POLLIN:
+                logging.debug('Received response on client socket')
+                result = self.zclient.recv_unicode()
+                logging.debug('Received message was: %s', result)
+                break
+            else:
+                # We didn't get a reply back from the server, let's retry
+                self.retries -= 1
+                logging.warning(
+                    'Did not receive response, retrying...'
+                )
+
+                # Socket is confused. Close and remove it.
+                logging.debug('Closing sockets and re-establishing connection...')
+                self.zclient.close()
+                self.zpoller.unregister(self.zclient)
+
+                # Re-establish the connection
+                logging.debug(
+                    'Re-establishing connection to endpoint: %s',
+                    self.endpoint
+                )
+                self.zclient = self.zcontext.socket(zmq.REQ)
+                self.zclient.connect(self.endpoint)
+                self.zclient.setsockopt(zmq.LINGER, 0)
+                self.zpoller.register(self.zclient, zmq.POLLIN)
+
+        # Close the socket and terminate the context
+        logging.debug('Closing sockets and exiting')
+        self.zclient.close()
+        self.zpoller.unregister(self.zclient)
+        self.zcontext.term()
+
+        # Did we have any result reply at all?
+        if result is None:
+            logging.error(
+                'Did not receive response, aborting...'
+            )
+            return {
+                'success': 1,
+                'msg': 'Did not receive response, aborting...'
+            }
+
+        return result
+
+
+class VPollerClientMessage(object):
+    # Message attribute types we expect to receive
+    # before we start processing a task request
+    msg_attr_types = {
+        'hostname': (types.StringType, types.UnicodeType),
+        'name': (types.StringType, types.UnicodeType, types.NoneType),
+        'key': (types.StringType, types.UnicodeType, types.NoneType),
+        'username': (types.StringType, types.UnicodeType, types.NoneType),
+        'password': (types.StringType, types.UnicodeType, types.NoneType),
+        'properties': (types.TupleType,  types.ListType, types.NoneType),
+    }
+
+    @classmethod
+    def validate_msg(cls, required, msg):
+        """
+        Helper method for validating a client message
+
+        Checks whether the required attributes are contained
+        within the received task request and also checks whether
+        they are from the proper type.
+
+        Returns:
+            True if the message has been successfully validated,
+            False otherwise
+
+        """
+        logging.debug(
+            'Validating client message, required to have: %s',
+            required
+        )
+
+        # Check if we have the required message attributes
+        if not all(k in msg for k in required):
+            logging.debug('Required message attributes are missing')
+            return False
+
+        # Check if we have correct types of the message attributes
+        for k in msg.keys():
+            if k not in cls.msg_attr_types:
+                continue
+            if not isinstance(msg[k], cls.msg_attr_types.get(k)):
+                logging.debug("Incorrect type for '%s' message attribute", k)
+                return False
+
+        logging.debug('Client message successfully validated')
+
+        return True
+
