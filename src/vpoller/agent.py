d776f4d36926841f098bae7017da870064d451e8
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-01-14 17:08:58 +0200

    Update copyright years

--- src/vpoller/agent.py
+++ src/vpoller/agent.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013-2014 Marin Atanasov Nikolov <dnaeon@gmail.com>
+# Copyright (c) 2013-2015 Marin Atanasov Nikolov <dnaeon@gmail.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
