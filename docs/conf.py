825a7c47574ef093048bbeaf35947dc04fd964fc
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-02-17 17:08:41 +0200

    Update copyright years

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'vPoller'
-copyright = u'2014, Marin Atanasov Nikolov'
+copyright = u'2013-2015, Marin Atanasov Nikolov'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
