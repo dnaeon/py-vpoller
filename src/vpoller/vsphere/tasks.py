2c94d4ca8ba458a79503be3f915809d3aa39fe16
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-05-10 17:02:55 +0300

    Issue #113: PEP-476: Disable SSL certificate verification by default

--- src/vpoller/vsphere/tasks.py
+++ src/vpoller/vsphere/tasks.py
@@ -32,6 +32,18 @@ which this module is using please check the link below:
 
 """
 
+
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
 import pyVmomi
 
 from vpoller.log import logger
