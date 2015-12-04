641eeddaffd60cef6211475b64373af47b581516
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-12-04 12:45:08 +0200

    Depend on new versions of pyVmomi and vconnector

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     install_requires=[
         'pyzmq >= 14.3.1',
         'docopt >= 0.6.2',
-        'pyvmomi >= 5.5.0-2014.1.1',
-        'vconnector >= 0.4.1',
+        'pyvmomi >= 6.0.0',
+        'vconnector >= 0.4.2',
     ]
 )
